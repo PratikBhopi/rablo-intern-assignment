7a7110f2398e2cae3f4b3aade94abad1064bc1ef
Pratik <pkbhopi132@gmail.com>
2025-03-14 23:19:39 +0530

    server updated

--- server/server.js
+++ server/server.js
@@ -7,10 +7,7 @@ const userRouter=require('./routes/user.routes')
 
 const app = express();
 app.use(express.json());
-app.use(cors({
-    origin: "http://localhost:5173",  
-    credentials: true 
-}));
+app.use(cors());
 app.get('/', (req, res) => {
     res.send(`Product API's!`);
 });
